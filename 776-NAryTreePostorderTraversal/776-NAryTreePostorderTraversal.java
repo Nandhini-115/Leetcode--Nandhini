96687c1f879a4f6e7a135c19c296913165bd5498
nandhini <nandhiniselvaraj9@gmail.com>
2026-08-11 14:17:18 +0530

    Create 776-NAryTreePostorderTraversal

---
+++ 776-NAryTreePostorderTraversal/776-NAryTreePostorderTraversal.java
@@ -0,0 +1,38 @@
+// Last updated: 11/08/2026, 14:17:17
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
+*/
+
+import java.util.*;
+
+class Solution {
+    public List<Integer> postorder(Node root) {
+        List<Integer> ans = new ArrayList<>();
+        helper(root, ans);
+        return ans;
+    }
+    public void helper(Node root, List<Integer> ans) {  
+        if(root == null) {
+            return;
+        }
+        for(Node child : root.children) {
+            helper(child, ans);
+        }
+        ans.add(root.val);
+    }
+}
