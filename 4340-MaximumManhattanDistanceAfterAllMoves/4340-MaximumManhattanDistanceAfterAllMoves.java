9928ac75d1be5d39c7ce2a0e257b7d2fb2e48eb3
nandhini <nandhiniselvaraj9@gmail.com>
2026-08-11 14:13:58 +0530

    Create 4340-MaximumManhattanDistanceAfterAllMoves

---
+++ 4340-MaximumManhattanDistanceAfterAllMoves/4340-MaximumManhattanDistanceAfterAllMoves.java
@@ -0,0 +1,19 @@
+// Last updated: 11/08/2026, 14:13:58
+class Solution {
+    public int maxDistance(String moves) {
+        int x=0,y=0,blank=0;
+        for(char ch:moves.toCharArray()){
+            if(ch=='U')
+                y++;
+            else if(ch=='D')
+                y--;
+            else if(ch=='L')
+                x--;
+            else if(ch=='R')
+                x++;
+            else
+                blank++;
+        }
+        return Math.abs(x)+Math.abs(y)+blank;
+    }
+}
