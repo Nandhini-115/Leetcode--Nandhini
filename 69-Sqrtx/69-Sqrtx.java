6d8681e569039f2e73cee6c0a995de8168f5e105
nandhini <nandhiniselvaraj9@gmail.com>
2026-08-11 14:22:04 +0530

    Create 69-Sqrtx

---
+++ 69-Sqrtx/69-Sqrtx.java
@@ -0,0 +1,22 @@
+// Last updated: 11/08/2026, 14:22:04
+class Solution {
+    public int mySqrt(int x) {
+        if(x == 0 || x == 1) {
+            return x;
+        }
+        int left = 1;
+        int right = x;
+        int ans = 0;
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            if(mid <= x / mid) {
+                ans = mid;       
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+        return ans;
+    }
+}
